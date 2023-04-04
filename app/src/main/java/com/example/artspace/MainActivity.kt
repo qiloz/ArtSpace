d224910ccf4b09c4975c0df2c6af04eadc2906ae
Alexander Ivanov <zuard@outlook.com>
2023-04-04 12:19:28 +0300

    feat: realise basic picture switcher

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ArtSpaceApp()
                 }
             }
         }
@@ -30,14 +41,139 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview(
+    showBackground = true,
+)
+fun ArtSpaceApp() {
+    ArtSpaceInstance(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight(0.8f)
+//            .wrapContentSize(Alignment.TopCenter)
+            .padding(10.dp)
+
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ArtSpaceTheme {
-        Greeting("Android")
+fun ArtSpaceInstance(
+    modifier: Modifier = Modifier
+) {
+    var numOfPic by remember {
+        mutableStateOf(1)
+    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround,
+    ) {
+
+        // description section
+        val robotoFont = FontFamily(
+            Font(R.font.roboto_light, FontWeight.Light)
+        )
+
+
+        Spacer(
+            modifier = Modifier
+                .padding(10.dp)
+//            .fillMaxHeight()
+        )
+        //Picture section
+        GetPicture(picIndex = numOfPic)
+//
+        Spacer(
+            modifier = Modifier
+                .padding(10.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .shadow(1.dp)
+                .fillMaxWidth(0.8f)
+                .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp)
+        ) {
+            Text(
+                text = "Title",         // TODO: Remove hardcode
+                fontSize = 25.sp,
+                fontFamily = robotoFont,
+                fontWeight = FontWeight.Light
+            )
+            Row {
+                Text(
+                    modifier = Modifier
+                        .padding(end = 5.dp),
+                    text = "Artist",    // TODO: Remove hardcode
+                    fontWeight = FontWeight(500)
+                )
+                Text(text = "(Year)")   // TODO: Remove hardcode
+
+                Text(text = "$numOfPic") // NOTE: DEBUG
+            }
+        }
+    }
+
+    Spacer(
+        modifier = Modifier
+            .height(5.dp)
+    )
+
+    // buttons section
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(0.8f),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        val buttonSize = Modifier.width(100.dp)
+        Button(onClick = {
+            numOfPic--
+        }, buttonSize) {
+            Text(text = "Previous")
+        }
+        Button(onClick = {
+            numOfPic++
+        }, buttonSize) {
+            Text(text = "Next")
+        }
     }
 }
+
+@Composable
+fun GetPicture(
+    picIndex: Int,
+) {
+    val pictureData = when (picIndex) {
+        1 -> R.drawable.picture_1
+        2 -> R.drawable.picture_2
+        3 -> R.drawable.picture_3
+        4 -> R.drawable.picture_4
+        5 -> R.drawable.picture_5
+        else -> R.drawable.picture_6
+    }
+    Box(
+        modifier = Modifier
+//            .wrapContentSize(align = Alignment.TopStart, unbounded = true)
+            .shadow(1.dp)
+
+    ) {
+        val img = painterResource(id = pictureData)
+        Image(
+            painter = img,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(10.dp)
+//                .wrapContentSize(align = Alignment.TopStart, unbounded = true)
+                .fillMaxSize(),
+
+            )
+
+    }
+}
+
+fun getRandomValue(
+    values: IntRange
+): Int {
+    return (values).random()
+}
+
