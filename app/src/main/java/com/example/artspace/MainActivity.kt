71239bbe569951c6ba91b0dc96858f53e9c7d113
Alexander Ivanov <sasha2108i@gmail.com>
2023-04-04 21:04:08 +0300

    feat: implement carousel picture switcher

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -6,15 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -30,8 +31,7 @@ class MainActivity : ComponentActivity() {
             ArtSpaceTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = Color(0xFFFFFFFF)
                 ) {
                     ArtSpaceApp()
                 }
@@ -48,10 +48,7 @@ fun ArtSpaceApp() {
     ArtSpaceInstance(
         modifier = Modifier
             .fillMaxSize()
-//            .fillMaxHeight(0.8f)
-//            .wrapContentSize(Alignment.TopCenter)
             .padding(10.dp)
-
     )
 }
 
@@ -68,66 +65,34 @@ fun ArtSpaceInstance(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
-
-        // description section
-        val robotoFont = FontFamily(
-            Font(R.font.roboto_light, FontWeight.Light)
-        )
-
         Spacer(
-            modifier = Modifier
-                .padding(10.dp)
-//            .fillMaxHeight()
+            modifier = Modifier.padding(10.dp)
         )
-        //Picture section
+        // Picture section
         GetPicture(picIndex = numOfPic)
-//
-        Spacer(
-            modifier = Modifier
-                .padding(10.dp)
-        )
-        Column(
-            modifier = Modifier
-                .shadow(1.dp)
-                .fillMaxWidth(0.8f)
-                .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp)
-        ) {
-            Text(
-                text = "Title",         // TODO: Remove hardcode
-                fontSize = 25.sp,
-                fontFamily = robotoFont,
-                fontWeight = FontWeight.Light
-            )
-            Row {
-                Text(
-                    modifier = Modifier
-                        .padding(end = 5.dp),
-                    text = "Artist",    // TODO: Remove hardcode
-                    fontWeight = FontWeight(500)
-                )
-                Text(text = "(Year)")   // TODO: Remove hardcode
 
-                Text(text = "$numOfPic") // NOTE: DEBUG
-            }
-        }
+        // Description section
         Spacer(
-            modifier = Modifier
-                .height(5.dp)
+            modifier = Modifier.padding(10.dp)
+        )
+        PrintDescription(picIndex = numOfPic)
+
+        Spacer(
+            modifier = Modifier.height(5.dp)
         )
         // buttons section
         Row(
-            modifier = Modifier
-                .fillMaxWidth(0.8f),
-            horizontalArrangement = Arrangement.SpaceBetween
+            modifier = Modifier.fillMaxWidth(0.8f), horizontalArrangement = Arrangement.SpaceBetween
         ) {
             val buttonSize = Modifier.width(100.dp)
             Button(onClick = {
-                numOfPic--
+                numOfPic = switchImage(numOfPic, maxItems = 6, inverse = true)
             }, buttonSize) {
                 Text(text = "Previous")
             }
+            Text(text = numOfPic.toString())
             Button(onClick = {
-                numOfPic++
+                numOfPic = switchImage(numOfPic, maxItems = 6, inverse = false)
             }, buttonSize) {
                 Text(text = "Next")
             }
@@ -147,39 +112,101 @@ fun GetPicture(
         5 -> R.drawable.picture_5
         else -> R.drawable.picture_6
     }
-    val img = painterResource(id = pictureData)
-    val sizeImgWidth = img.intrinsicSize.width
-    val sizeImgHeight = img.intrinsicSize.height
 
     Box(
-        modifier = Modifier
-//            .wrapContentSize(align = Alignment.TopStart, unbounded = true)
-            .shadow(1.dp)
-
-
+        modifier = Modifier.shadow(1.dp)
     ) {
-
+        val img = painterResource(id = pictureData)
+        val sizeImgWidth = img.intrinsicSize.width
+        val sizeImgHeight = img.intrinsicSize.height
         Image(
             painter = img,
             contentDescription = null,
             contentScale = ContentScale.FillBounds,
             modifier = Modifier
                 .padding(10.dp)
-//                .wrapContentSize(align = Alignment.TopStart, unbounded = true)
-//                .fillMaxSize()
-//                .fillMaxWidth(sizeImg.width)
                 .widthIn(min = sizeImgWidth.dp / 2, max = sizeImgWidth.dp)
-                .heightIn(min = sizeImgHeight.dp / 2.4f, max  = sizeImgHeight.dp)
-
+                .heightIn(min = sizeImgHeight.dp / 2.4f, max = sizeImgHeight.dp)
         )
-//        Text(text = sizeImgWidth.toString())
     }
-
 }
 
+@Composable
+fun PrintDescription(
+    picIndex: Int
+) {
+    val robotoFont = FontFamily(
+        Font(R.font.roboto_light, FontWeight.Light)
+    )
+    val title = when (picIndex) {
+        1 -> stringResource(id = R.string.title_1)
+        2 -> stringResource(id = R.string.title_2)
+        3 -> stringResource(id = R.string.title_3)
+        4 -> stringResource(id = R.string.title_4)
+        5 -> stringResource(id = R.string.title_5)
+        else -> stringResource(id = R.string.title_6)
+    }
+
+    val artist = when (picIndex) {
+        1 -> stringResource(id = R.string.artist_1)
+        2 -> stringResource(id = R.string.artist_2)
+        3 -> stringResource(id = R.string.artist_3)
+        4 -> stringResource(id = R.string.artist_4)
+        5 -> stringResource(id = R.string.artist_5)
+        else -> stringResource(id = R.string.artist_6)
+    }
+
+    val year = when (picIndex) {
+        1 -> stringResource(id = R.string.year_1)
+        2 -> stringResource(id = R.string.year_2)
+        3 -> stringResource(id = R.string.year_3)
+        4 -> stringResource(id = R.string.year_4)
+        5 -> stringResource(id = R.string.year_5)
+        else -> stringResource(id = R.string.year_6)
+    }
+
+    Column(
+        modifier = Modifier
+            .shadow(1.dp)
+            .fillMaxWidth(0.8f)
+            .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp)
+    ) {
+        Text(
+            text = title, fontSize = 25.sp, fontFamily = robotoFont, fontWeight = FontWeight.Light
+        )
+        Row {
+            Text(
+                modifier = Modifier.padding(end = 5.dp), text = artist, fontWeight = FontWeight(500)
+            )
+            Text(text = year)
+        }
+    }
+}
+
+fun switchImage(
+    value: Int = 1, maxItems: Int = 1, inverse: Boolean = false
+): Int {
+    var updatedValue: Int = value
+    if (!inverse) {
+        updatedValue++
+        if (updatedValue > maxItems) {
+            return 1
+        } else {
+            return updatedValue
+        }
+    } else {
+        updatedValue--
+        if (updatedValue < 1) {
+            return maxItems
+        } else {
+            return updatedValue
+        }
+    }
+}
+
+// TODO: Make open app with random picture with function below
 fun getRandomValue(
     values: IntRange
 ): Int {
     return (values).random()
 }
-
